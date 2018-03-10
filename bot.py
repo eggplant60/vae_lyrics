b475b32ba5083bfeb72d3a02e5a6e6ad0dc1957e
eggplant60 <nagoyan240@gmail.com>
2018-03-10 21:57:07 +0900

    [fix] 英単語の間にスペースが入らない不具合の修正、他
    - 1度に複数歌詞を生成できるように、引数を追加
    - CPUで計算するように変更

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from extract_vector_vae import *
 
 def generate_lyrics(result_dir, batchsize):
 
-    source_ids, _, _, _, test_source, _, model = load_model_vocab(result_dir)
+    source_ids, _, _, _, test_source, _, model = load_model_vocab(result_dir, gpu=-1)
 
     source_words = {i: w for w, i in source_ids.items()}
 
@@ -24,20 +24,42 @@ def generate_lyrics(result_dir, batchsize):
 
     lyrics = []
     for i, result in enumerate(results):
-        decode_string = ''.join([source_words[x] for x in result]).replace('/','\n')
+        #decode_string = ''.join([source_words[x] for x in result]).replace('/','\n')
+        decode_string = words2sentence([source_words[x] for x in result])
         lyrics.append(decode_string)
     
     return lyrics
 
 
+def is_alphabet(word):
+    return all([65 <= ord(char) <= 122  for char in word])
+
+
+def words2sentence(words):
+    len_words = len(words)
+    sentence = words[0]
+    for i in range(len_words-1):
+        if words[i+1] == '/':
+            sentence += '\n'
+        elif is_alphabet(words[i]) \
+             and is_alphabet(words[i+1]) \
+             and words[i] != '/':
+            sentence += (' ' + words[i+1])
+        else:
+            sentence += words[i+1]
+    return sentence
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='search lyrics similar to the query')
-    parser.add_argument('--quiet', '-q', action='store_true')
-    parser.add_argument('--num', '-n', type=int, default=1)
+    parser.add_argument('--quiet', '-q', action='store_true',
+                        help='generate sentence\(s\) without tweet')
+    parser.add_argument('--num', '-n', type=int, default=1,
+                        help='number of sentences generated')
     args = parser.parse_args()
 
     # Load Model and generate lyrics
-    bodies = generate_lyrics('/home/naoya/work/text/vae_lyrics/result_0208_C041', args.num)
+    bodies = generate_lyrics('/home/naoya/work/text/vae_lyrics/result_0209_C024', args.num)
     for i in range(args.num):
         print(bodies[i])
         print()
