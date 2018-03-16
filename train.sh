7a79cf68fb251b2aa6542e1e5d3aaeee826d59d6
eggplant60 <nagoyan240@gmail.com>
2018-03-17 00:49:41 +0900

    [rename] lstm_vae.sh -> train.sh

---
+++ train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dir='data_utanet'
+
+./lstm_vae.py -u 400 \
+	      -t gru \
+	      -l 2 \
+	      -e 50 \
+	      -b 10 \
+	      --n_embed 400 \
+	      --word_dropout 0.40 \
+	      --denoising_rate 0.0 \
+	      --n_latent 100 \
+	      --validation-interval 2000 \
+	      --log-interval 200 \
+	      --max-source-sentence 30 \
+	      --validation-source $dir/test.txt \
+	      --validation-target $dir/test.txt \
+	      $dir/train.txt $dir/train.txt \
+	      $dir/train.vocab $dir/train.vocab
+
+
+#--resume result_0213_20000/model_iter_134444.npz \
